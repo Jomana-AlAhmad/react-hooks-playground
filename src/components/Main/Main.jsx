0d9a524aff6073dae508f2e7efaad16e11fc423c
Jomana <jomana.alahmad@sky.uk>
2019-11-26 20:03:10 +0000

    use usestate hooks

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,26 +1,15 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { MainWrapper, Button } from './Main.style';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            toggle: false
-        };
-    }
-
-    switchToggle = () => {
-        this.setState({
-            toggle: !this.state.toggle
-        });
-    };
+function Main () {
+    const [toggle,setToggle] = useState(false);
 
     render() {
         return (
             <MainWrapper>
                 <h2>EX1_useState_basic</h2>
-                <p>{`the light is ${this.state.toggle ? 'ON' : 'OFF'}`}</p>
-                <Button onClick={this.switchToggle}>TOGGLE</Button>
+                <p>{`the light is ${toggle ? 'ON' : 'OFF'}`}</p>
+                <Button onClick={() => setToggle (!toggle)}>TOGGLE</Button>
             </MainWrapper>
         );
     }
