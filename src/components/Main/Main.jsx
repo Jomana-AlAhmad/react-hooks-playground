d39f46e71d4642903939b68274b151a6c53b069c
Jomana <jomana.alahmad@sky.uk>
2019-11-27 08:53:50 +0000

    add hooks

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,38 +1,35 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { MainWrapper, Button, List, ListItem } from './Main.style';
 
-class Main extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            firstName: 'Joe',
-            lastName: 'Smith',
-            age: 38
-        };
-    }
+function Main () {
+    const [person, setPerson] = useState ({
+        firstName: 'Joe',
+        lastName: 'Smith',
+        age: 38
+    })
 
     changeName = () => {
-        this.setState({
+       setPerson(prevState => ({
+           ...prevState,
             firstName: 'Steve'
-        });
+        }));
     };
-
-    render() {
+ 
         return (
             <MainWrapper>
                 <h2>EX2_useState_intermediate</h2>
                 <p>my credentials are:</p>
                 <List>
-                    {Object.keys(this.state).map(credential => (
+                    {Object.keys(person).map(credential => (
                         <ListItem>
-                            {`${credential}: ${this.state[credential]}`}
+                            {`${credential}: ${person[credential]}`}
                         </ListItem>
                     ))}
                 </List>
-                <Button onClick={this.changeName}>CHANGE NAME</Button>
+                <Button onClick={changeName}>CHANGE NAME</Button>
             </MainWrapper>
         );
     }
-}
+
 
 export default Main;
